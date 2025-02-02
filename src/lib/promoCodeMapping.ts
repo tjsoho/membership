65402cc70a6f69e4b0d4c0c15afa1f189445d50e
Tjcarroll1 <tjcarroll1@icloud.com>
2025-02-02 16:40:41 +1000

    added new promo code

--- src/lib/promoCodeMapping.ts
+++ src/lib/promoCodeMapping.ts
@@ -1,5 +1,5 @@
 export const PROMO_CODES = {
-  'TOBYRATES101': 'promo_1QnexaKYNGRKelfX23gRFeXB',
+  'TOBYRATES101': 'promo_1QnwpcKYNGRKelfX9fgwLaME',
   'EARLYBIRD': 'promo_another_id_here',
   'SPECIAL50': 'promo_different_id_here',
   // Add more codes as needed
